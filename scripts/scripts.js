21b7e83cf68fee4db47821f27e802ddc5af4bf41
Pdsnowden <paulsnowden@outlook.com>
2018-07-30 22:34:43 -0700

    Added timer
    Added the 60 second countdown clock and played with template literals
    (date in footer). Next is to add the timer start/reset button and then
    tie it to the clock. After that...i'll come to that bridge later

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,3 +7,31 @@ function changeImg() {
     img.src = "images/animal.png";
     console.log(img);
 }
+
+const TODAY = new Date();
+const YEAR = TODAY.getFullYear();
+const FOOTERTEXT = document.querySelector(".date");
+
+FOOTERTEXT.innerHTML = (`Copyright ${YEAR}`);
+
+
+addEventListener('load', function () {
+    var ONE_SECOND = 1000,
+        lastTick = +new Date(),
+        timer = document.querySelector(".timer");
+        seconds = 60;
+
+    function tick() {
+        var now = +new Date(),
+            nextTick = 2 * ONE_SECOND - (now - lastTick);
+
+        lastTick = now;
+        if (--seconds) {
+            setTimeout(tick, nextTick > ONE_SECOND ? ONE_SECOND : nextTick);
+        }
+        timer.innerHTML = "0:" + (seconds < 10 ? '0' : '') + seconds;
+    }
+
+    timer.innerHTML = "01:00";
+    setTimeout(tick, ONE_SECOND);
+});
