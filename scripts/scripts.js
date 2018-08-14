3b2c2b0ff4972c753ba033509e818d4abf241976
Pdsnowden <paulsnowden@outlook.com>
2018-08-13 22:37:50 -0700

    Really monkey'd around in there and screwed it up

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,109 +17,38 @@ const MUPPETS = [
 
 // MAIN FUNCTION
 function startGame() {
-  var gameArry = scrambleArray();
+  var gameArray = scrambleArray();
 
 };
 
 // SCRAMBLES MUPPET ARRAY
 function scrambleArray() {
   var finishedScramble = [];
-  var muppetsCopy = MUPPETS;
+  var muppetsCopy = [];
+
+  // CLONES FUNCTION 
+  function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+      if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+  }
+
+ muppetsCopy = clone(MUPPETS);
 
   while (muppetsCopy.length !== 0) {
     let randomIndex = Math.floor(Math.random() * muppetsCopy.length);
     finishedScramble.push(muppetsCopy[randomIndex]);
     muppetsCopy.splice(randomIndex, 1);
   }
+  return finishedScramble;
 };
 
 
 
 
-// TESTING***  CHANGES THE IMAGE ONCLICK
-document.querySelector('img').addEventListener('click', changeImg);
-
-function changeImg () {
-  let img = document.querySelector('img');
-
-  img.src = 'images/animal.png';
-}
-
-// TIMER
-const TODAY = new Date();
-const YEAR = TODAY.getFullYear();
-const FOOTERTEXT = document.querySelector('.date');
-
-FOOTERTEXT.innerHTML = (`Copyright ${YEAR}`);
-
-addEventListener('load', function () {
-  let ONE_SECOND = 1000;
-  let seconds = 60;
-  lastTick = +new Date(),
-  timer = document.querySelector('.timer');
-
-  function tick () {
-    let now = +new Date(),
-      nextTick = 2 * ONE_SECOND - (now - lastTick);
-
-    lastTick = now;
-    if (--seconds) {
-      setTimeout(tick, nextTick > ONE_SECOND ? ONE_SECOND : nextTick);
-    }
-    timer.innerHTML = '0:' + (seconds < 10 ? '0' : '') + seconds;
-  }
-
-  timer.innerHTML = '01:00';
-  setTimeout(tick, ONE_SECOND);
-});
-
-// TEXT BOX
-
-const USERTEXT = document.querySelector('.textAnswer');
-
-// Clear box on click
-function boxClicked () {
-  clearTextBox();
-}
-// gets user guesss on enter
-function handleEnter(e) {
-  e = e || window.event;
-  if(e.keyCode === 13) {
-    var elem = e.srcElement || e.target;
-    console.log(elem.value);
-    clearTextBox();
-  }
-}
-
-function clearTextBox() {
-  USERTEXT.value = '';
-}
-
-n = (MUPPETS.length);
-
-var k = "";
-
-for (i = 0; i < n; i++){
-  var list = MUPPETS[i].name;
-  if (i % 4 === 0){
-    if (i === 0){
-      k += "<li>" +list+"</li>"; 
-    } else {
-    k += "<br>"  + "<li>" +list+"</li>";
-    }
-  } else {
-    k += "<li>" +list+"</li>"; 
-  }
-}
-
-var myList = document.querySelector('.names');
-myList.innerHTML = k;
-
-
-// SHUFFLE ARRAY ?
-
-var shuffleArrayPhoto = MUPPETS.sort(function() { return 0.5 - Math.random() });
-var shuffleArrayList = MUPPETS.sort(function() { return 0.5 - Math.random() });
 
 
 
